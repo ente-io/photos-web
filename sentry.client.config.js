1f6adcc7bab0938d28f72ef2384bd2f637f408f3
Abhinav-grd <abhinavk.grd@gmail.com>
2021-06-10 09:58:29 +0530

    send  npm package version as relases version with sentry

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,15 +1,20 @@
 import * as Sentry from '@sentry/nextjs';
+import { CaptureConsole as CaptureConsoleIntegration } from '@sentry/integrations';
 
-const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN ?? 'https://860186db60c54c7fbacfe255124958e8@errors.ente.io/4';
+const SENTRY_ENV = process.env.SENTRY_ENV ?? 'development';
 
 Sentry.init({
     dsn: SENTRY_DSN,
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
     tracesSampleRate: 1.0,
-    release: 'bada-frame-v' + process.env.npm_package_version,
-    environment: process.env.NODE_ENV,
+    environment: SENTRY_ENV,
+    release: process.env.SENTRY_RELEASE,
     attachStacktrace: true,
+    integrations: [new CaptureConsoleIntegration(
+        {
+            levels: ['error'],
+        },
+    )],
     // ...
     // Note: if you want to override the automatic release value, do not set a
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,16 +1,11 @@
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN ?? 'https://860186db60c54c7fbacfe255124958e8@errors.ente.io/4';
+const SENTRY_ENV = process.env.SENTRY_ENV ?? 'development';
 
 Sentry.init({
     dsn: SENTRY_DSN,
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
     tracesSampleRate: 1.0,
-    release: 'bada-frame-v' + process.env.npm_package_version,
-    environment: process.env.NODE_ENV,
-    // ...
-    // Note: if you want to override the automatic release value, do not set a
-    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-    // that it will also get attached to your source maps
+    environment: SENTRY_ENV,
+    release: process.env.SENTRY_RELEASE,
 });
