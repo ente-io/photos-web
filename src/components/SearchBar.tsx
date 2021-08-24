7077f04199ad225622948a49231cc4b5ff2af984
Abhinav-grd <abhinavk.grd@gmail.com>
2021-08-24 13:27:43 +0530

    remove window size variable used for size control

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import { Search, SearchStats, SetCollections } from 'pages/gallery';
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import AsyncSelect from 'react-select/async';
 import { components } from 'react-select';
@@ -19,38 +19,28 @@ import DateIcon from './icons/DateIcon';
 import SearchIcon from './icons/SearchIcon';
 import CrossIcon from './icons/CrossIcon';
 
-const Wrapper = styled.div<{ width: number; isDisabled: boolean }>`
+const Wrapper = styled.div<{ isDisabled: boolean }>`
     position: fixed;
-    z-index: 1000;
     top: 0;
-    left: ${(props) => `max(0px, 50% - min(360px,${props.width / 2}px))`};
+    z-index: 1000;
     width: 100%;
-    max-width: 720px;
     display: flex;
     align-items: center;
-    justify-content: center;
-    padding: 0 5%;
-    background-color: #111;
-    color: #fff;
     min-height: 64px;
     transition: opacity 1s ease;
     opacity: ${(props) => (props.isDisabled ? 0 : 1)};
     margin-bottom: 10px;
 `;
 
-const SearchButton = styled.div<{ isDisabled: boolean }>`
-    top: 1px;
-    z-index: 100;
-    right: 80px;
-    color: #fff;
-    cursor: pointer;
-    transition: opacity 1s ease;
-    opacity: ${(props) => (props.isDisabled ? 0 : 1)};
-    min-height: 64px;
-    position: fixed;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+const SearchButton = styled.div<{ isOpen: boolean }>`
+    display: none;
+    @media (max-width: 640px) {
+        display: ${({ isOpen }) => (!isOpen ? 'inherit' : 'none')};
+        padding: 0 20px;
+        right: 80px;
+        cursor: pointer;
+        position: fixed;
+    }
 `;
 
 const SearchStatsContainer = styled.div`
@@ -59,6 +49,20 @@ const SearchStatsContainer = styled.div`
     align-items: center;
     color: #979797;
     margin-bottom: 8px;
+`;
+
+const SearchInput = styled.div<{ isOpen: boolean }>`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    background: #111;
+    @media (max-width: 640px) {
+        display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
+        width: 100%;
+        padding: 0 20px;
+    }
+    width: 484px;
+    margin: auto;
 `;
 
 export enum SuggestionType {
@@ -86,7 +90,6 @@ interface Props {
     searchStats: SearchStats;
 }
 export default function SearchBar(props: Props) {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
     const selectRef = useRef(null);
     useEffect(() => {
         if (props.isOpen) {
@@ -96,11 +99,6 @@ export default function SearchBar(props: Props) {
         }
     }, [props.isOpen]);
 
-    useEffect(() => {
-        window.addEventListener('resize', () =>
-            setWindowWidth(window.innerWidth)
-        );
-    });
     // = =========================
     // Functionality
     // = =========================
@@ -273,8 +271,8 @@ export default function SearchBar(props: Props) {
                     {constants.SEARCH_STATS(props.searchStats)}
                 </SearchStatsContainer>
             )}
-            {windowWidth > 1000 || props.isOpen ? (
-                <Wrapper isDisabled={props.isFirstFetch} width={windowWidth}>
+            <Wrapper isDisabled={props.isFirstFetch}>
+                <SearchInput isOpen={props.isOpen}>
                     <div
                         style={{
                             flex: 1,
@@ -304,14 +302,13 @@ export default function SearchBar(props: Props) {
                             </div>
                         )}
                     </div>
-                </Wrapper>
-            ) : (
+                </SearchInput>
                 <SearchButton
-                    isDisabled={props.isFirstFetch}
+                    isOpen={props.isOpen}
                     onClick={() => !props.isFirstFetch && props.setOpen(true)}>
                     <SearchIcon />
                 </SearchButton>
-            )}
+            </Wrapper>
         </>
     );
 }
