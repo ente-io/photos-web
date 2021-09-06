98c5da7577462ebd52b0df194a5cb2cd6232b9aa
Abhinav-grd <abhinavk.grd@gmail.com>
2021-09-06 15:14:44 +0530

    testting workerfs

--- src/services/ffmpeg/ffmpegClient.ts
+++ src/services/ffmpeg/ffmpegClient.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { createFFmpeg, FFmpeg } from 'ffmpeg';
 
-const WORKING_DIR = 'working';
+const WORKING_DIR = '/working';
 const WORKERFS = 'WORKERFS';
 
 class FFmpegClient {
@@ -54,24 +55,47 @@ class FFmpegClient {
                     thumb = this.ffmpeg.FS('readFile', thumbFilePath);
                     break;
                 } catch (e) {
+                    console.log(e);
+                    await new Promise((resolve) => {
+                        setTimeout(() => resolve(null), 100000);
+                    });
                     seekTime = Number((seekTime / 10).toFixed(3));
                 }
             }
             return thumb;
+        } catch (e) {
+            console.log(e);
+            throw e;
         } finally {
-            this.cleanUp();
+            // this.cleanUp();
         }
     }
 
     private loadFile(inputFileName: string, file: File) {
-        const FS = this.ffmpeg.FS;
-        if (!this.workingDirectoryExists) {
-            FS('mkdir', WORKING_DIR);
-            this.workingDirectoryExists = true;
-        }
-        const tmpfile = new File([file], inputFileName, { type: file.type });
+        try {
+            const FS = this.ffmpeg.FS;
+            if (!this.workingDirectoryExists) {
+                FS('mkdir', WORKING_DIR);
+                this.workingDirectoryExists = true;
+            }
+            const blob = new Blob(['blob data']);
+            // const tmpfile = new File([file], inputFileName, {
+            //     type: file.type,
+            // });
+            // console.log(tmpfile);
 
-        FS('mount', WORKERFS, { files: [tmpfile] }, WORKING_DIR);
+            FS(
+                'mount',
+                WORKERFS,
+                { blobs: [{ name: 'blob.txt', data: blob }] },
+                WORKING_DIR
+            );
+            const x = FS('readFile', 'blob.txt');
+            console.log('read output', x);
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
     }
 
     private cleanUp() {
