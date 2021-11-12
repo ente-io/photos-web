89e2c42e666ada34123966bce37ee0d2fa31cbc8
Abhinav <abhinavk.grd@gmail.com>
2021-11-12 16:45:53 +0530

    temp

--- src/services/ffmpegService.ts
+++ src/services/ffmpegService.ts
@@ -1,37 +1,14 @@
-import { createFFmpeg, FFmpeg } from '@ffmpeg/ffmpeg';
 import { CustomError } from 'utils/common/errorUtil';
 import { logError } from 'utils/sentry';
 import QueueProcessor from './upload/queueProcessor';
 import { getUint8ArrayView } from './upload/readFileService';
-
+import ffmpeg from 'ffmpeg.js/ffmpeg-mp4';
 class FFmpegService {
-    private ffmpeg: FFmpeg = null;
-    private isLoading = null;
-
     private generateThumbnailProcessor = new QueueProcessor<Uint8Array>(1);
-    async init() {
-        try {
-            this.ffmpeg = createFFmpeg({
-                corePath: '/js/ffmpeg/ffmpeg-core.js',
-            });
-            this.isLoading = this.ffmpeg.load();
-            await this.isLoading;
-            this.isLoading = null;
-        } catch (e) {
-            logError(e, 'ffmpeg load failed');
-            throw e;
-        }
-    }
 
     async generateThumbnail(file: File) {
-        if (!this.ffmpeg) {
-            await this.init();
-        }
-        if (this.isLoading) {
-            await this.isLoading;
-        }
-        const response = this.generateThumbnailProcessor.queueUpRequest(
-            generateThumbnailHelper.bind(null, this.ffmpeg, file)
+        const response = this.generateThumbnailProcessor.queueUpRequest(() =>
+            this.generateThumbnailHelper(file)
         );
 
         const thumbnail = await response.promise;
@@ -40,44 +17,49 @@ class FFmpegService {
         }
         return thumbnail;
     }
-}
 
-async function generateThumbnailHelper(ffmpeg: FFmpeg, file: File) {
-    try {
-        const inputFileName = `${Date.now().toString()}-${file.name}`;
-        const thumbFileName = `${Date.now().toString()}-thumb.jpeg`;
-        ffmpeg.FS(
-            'writeFile',
-            inputFileName,
-            await getUint8ArrayView(new FileReader(), file)
-        );
-        let seekTime = 1.0;
-        let thumb = null;
-        while (seekTime > 0) {
-            try {
-                await ffmpeg.run(
+    async generateThumbnailHelper(file: File) {
+        try {
+            const inputFileName = `${Date.now().toString()}-${file.name}`;
+            const thumbFileName = `${Date.now().toString()}-thumb.png`;
+            let stdout = '';
+            let stderr = '';
+            const result = ffmpeg({
+                MEMFS: [
+                    {
+                        name: inputFileName,
+                        data: await getUint8ArrayView(new FileReader(), file),
+                    },
+                ],
+                arguments: [
                     '-i',
                     inputFileName,
                     '-ss',
-                    `00:00:0${seekTime.toFixed(3)}`,
+                    `00:00:01`,
                     '-vframes',
                     '1',
                     '-vf',
                     'scale=-1:720',
-                    thumbFileName
-                );
-                thumb = ffmpeg.FS('readFile', thumbFileName);
-                ffmpeg.FS('unlink', thumbFileName);
-                break;
-            } catch (e) {
-                seekTime = Number((seekTime / 10).toFixed(3));
-            }
+                    thumbFileName,
+                ],
+                print: function (data) {
+                    stdout += data + '\n';
+                },
+                printErr: function (data) {
+                    stderr += data + '\n';
+                },
+                onExit: function (code) {
+                    console.log('Process exited with code ' + code);
+                    console.log(stdout);
+                    console.log(stderr);
+                },
+            });
+            console.log(result);
+            return result.MEMFS[0].data;
+        } catch (e) {
+            logError(e, 'ffmpeg thumbnail generation failed');
+            throw e;
         }
-        ffmpeg.FS('unlink', inputFileName);
-        return thumb;
-    } catch (e) {
-        logError(e, 'ffmpeg thumbnail generation failed');
-        throw e;
     }
 }
 
