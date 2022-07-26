fe9e7ce01a1e6a12b2dd15b62bfce110db2d8382
Abhinav <abhinavk.grd@gmail.com>
2022-07-26 13:23:18 +0530

    move sentry user id generation util to sentry/user

--- src/utils/sentry/index.ts
+++ src/utils/sentry/index.ts
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/nextjs';
-import { getUserAnonymizedID } from 'utils/user';
+import { getUserAnonymizedID } from './user';
 
 export const logError = (
     error: any,
--- src/utils/user/index.ts
+++ src/utils/user/index.ts
@@ -5,29 +5,7 @@ import {
 } from 'types/user';
 import { getActualKey } from 'utils/common/key';
 import CryptoWorker from 'utils/crypto';
-import { getData, LS_KEYS, setData } from 'utils/storage/localStorage';
-
-export function makeID(length) {
-    let result = '';
-    const characters =
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(
-            Math.floor(Math.random() * charactersLength)
-        );
-    }
-    return result;
-}
-
-export function getUserAnonymizedID() {
-    let anonymizeUserID = getData(LS_KEYS.AnonymizeUserID)?.id;
-    if (!anonymizeUserID) {
-        anonymizeUserID = makeID(6);
-        setData(LS_KEYS.AnonymizeUserID, { id: anonymizeUserID });
-    }
-    return anonymizeUserID;
-}
+import { getData, LS_KEYS } from 'utils/storage/localStorage';
 
 export function getLocalUserDetails(): UserDetails {
     return getData(LS_KEYS.USER_DETAILS)?.value;
