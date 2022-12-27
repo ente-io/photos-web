e62a6b2ea7d71187c4b5c97c38384960307fea12
Abhinav <abhinavk.grd@gmail.com>
2022-12-27 18:27:59 +0530

    disable pipeConsoleLogsToDebugLogs

--- src/worker/machineLearning.worker.ts
+++ src/worker/machineLearning.worker.ts
@@ -2,14 +2,14 @@ import { expose } from 'comlink';
 import { EnteFile } from 'types/file';
 import mlService from 'services/machineLearning/machineLearningService';
 import { MachineLearningWorker } from 'types/machineLearning';
-import { pipeConsoleLogsToDebugLogs } from 'utils/logging';
+// import { pipeConsoleLogsToDebugLogs } from 'utils/logging';
 // import ReverseProxiedElectronCacheStorageProxy from './electronCacheStorageProxy.proxy';
 // import { setupResponseComlinkTransferHandler } from 'utils/comlink';
 
 export class DedicatedMLWorker implements MachineLearningWorker {
     constructor() {
         console.log('DedicatedMLWorker constructor called');
-        pipeConsoleLogsToDebugLogs();
+        // pipeConsoleLogsToDebugLogs();
         // this.init();
     }
 
