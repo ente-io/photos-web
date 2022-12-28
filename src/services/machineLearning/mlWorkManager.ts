5b13d76e6c4f83614f12ce0fb24e115c72b7fd42
Abhinav <abhinavk.grd@gmail.com>
2022-12-28 16:12:01 +0530

    updated mlWorkManager to use ComlinkWorker util

--- src/services/machineLearning/mlWorkManager.ts
+++ src/services/machineLearning/mlWorkManager.ts
@@ -6,10 +6,12 @@ import { FILE_TYPE } from 'constants/file';
 import { getToken } from 'utils/common/key';
 import { logQueueStats } from 'utils/machineLearning';
 import { getMLSyncJobConfig } from 'utils/machineLearning/config';
-import { MLWorkerWithProxy } from 'utils/machineLearning/worker';
 import { logError } from 'utils/sentry';
 import mlIDbStorage from 'utils/storage/mlIDbStorage';
 import { MLSyncJobResult, MLSyncJob } from './mlSyncJob';
+import { ComlinkWorker } from 'utils/comlink/comlinkWorker';
+import { DedicatedMLWorker } from 'worker/machineLearning.worker';
+import { getDedicatedMLWorker } from 'utils/comlink/ml';
 
 const LIVE_SYNC_IDLE_DEBOUNCE_SEC = 30;
 const LIVE_SYNC_QUEUE_TIMEOUT_SEC = 300;
@@ -17,13 +19,13 @@ const LOCAL_FILES_UPDATED_DEBOUNCE_SEC = 30;
 
 class MLWorkManager {
     private mlSyncJob: MLSyncJob;
-    private syncJobWorker: MLWorkerWithProxy;
+    private syncJobWorker: ComlinkWorker<typeof DedicatedMLWorker>;
 
     private debouncedLiveSyncIdle: () => void;
     private debouncedFilesUpdated: () => void;
 
     private liveSyncQueue: PQueue;
-    private liveSyncWorker: MLWorkerWithProxy;
+    private liveSyncWorker: ComlinkWorker<typeof DedicatedMLWorker>;
     private mlSearchEnabled: boolean;
 
     constructor() {
@@ -140,10 +142,10 @@ class MLWorkManager {
     // Live Sync
     private async getLiveSyncWorker() {
         if (!this.liveSyncWorker) {
-            this.liveSyncWorker = new MLWorkerWithProxy('ml-sync-live');
+            this.liveSyncWorker = getDedicatedMLWorker('ml-live-sync-worker');
         }
 
-        return this.liveSyncWorker.proxy;
+        return await new this.liveSyncWorker.remote();
     }
 
     private async terminateLiveSyncWorker() {
@@ -151,7 +153,7 @@ class MLWorkManager {
             return;
         }
         try {
-            const liveSyncWorker = await this.liveSyncWorker.proxy;
+            const liveSyncWorker = await new this.liveSyncWorker.remote();
             await liveSyncWorker.closeLocalSyncContext();
         } catch (error) {
             console.error(
@@ -187,10 +189,10 @@ class MLWorkManager {
     // Sync Job
     private async getSyncJobWorker() {
         if (!this.syncJobWorker) {
-            this.syncJobWorker = new MLWorkerWithProxy('ml-sync-job');
+            this.syncJobWorker = getDedicatedMLWorker('ml-sync-worker');
         }
 
-        return this.syncJobWorker.proxy;
+        return await new this.syncJobWorker.remote();
     }
 
     private terminateSyncJobWorker() {
