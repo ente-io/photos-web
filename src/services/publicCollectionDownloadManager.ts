b74badd9c0f3b0eda65b45484ef9d93247f80949
Abhinav <abhinavk.grd@gmail.com>
2023-01-23 23:33:07 +0530

    fix build

--- src/services/publicCollectionDownloadManager.ts
+++ src/services/publicCollectionDownloadManager.ts
@@ -136,10 +136,10 @@ class PublicCollectionDownloadManager {
                     passwordToken,
                     file
                 );
-                const fileBlob = await new Response(fileStream).blob();
                 if (forPreview) {
-                    return await getRenderableFileURL(file, fileBlob);
+                    return await getRenderableFileURL(file, fileStream);
                 } else {
+                    const fileBlob = await new Response(fileStream).blob();
                     const fileURL = await createTypedObjectURL(
                         fileBlob,
                         file.metadata.title
