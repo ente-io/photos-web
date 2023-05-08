3cb143171bdc5875eec5bbac4108ee6d839dd95f
Abhinav <abhinavk.grd@gmail.com>
2023-05-08 17:34:48 +0530

    revert image type changes

--- src/types/image/index.ts
+++ src/types/image/index.ts
@@ -1,9 +1,9 @@
-export interface BlobOptions {
-    type?: string;
-    quality?: number;
-}
-
 export interface Dimensions {
     width: number;
     height: number;
+}
+
+export interface BlobOptions {
+    type?: string;
+    quality?: number;
 }
