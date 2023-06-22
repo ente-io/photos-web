1d3c02dd1df0b22b371a2f0c9ea90d16e919c9ca
httpjamesm <github@httpjames.space>
2023-06-21 21:46:39 -0400

    feat: rename file modal

--- apps/locker/src/components/pages/locker/RenameFileModal.tsx
+++ apps/locker/src/components/pages/locker/RenameFileModal.tsx
@@ -1,45 +1,89 @@
-import { FlexWrapper } from '@/components/Container';
-import DialogTitleWithCloseButton from '@/components/DialogBox/TitleWithCloseButton';
-import { CollectionType } from '@/constants/collection';
-import { createCollection } from '@/services/collectionService';
-import { Button, Dialog, DialogContent, Input } from '@mui/material';
-import { useState } from 'react';
+import DialogBoxV2 from '@/components/DialogBoxV2';
+import SingleInputForm, {
+    SingleInputFormProps,
+} from '@/components/SingleInputForm';
+import { LockerDashboardContext } from '@/pages/locker';
+import { updateFilePublicMagicMetadata } from '@/services/fileService';
+import {
+    changeFileName,
+    splitFilenameAndExtension,
+    updateExistingFilePubMetadata,
+} from '@/utils/file';
+import { t } from 'i18next';
+import { useContext, useEffect, useState } from 'react';
 
 interface IProps {
     show: boolean;
     onHide: () => void;
 }
 
+const getFileTitle = (filename, extension) => {
+    if (extension) {
+        return filename + '.' + extension;
+    } else {
+        return filename;
+    }
+};
+
 const RenameFileModal = (props: IProps) => {
-    const [newName, setNewName] = useState('');
+    const { selectedFiles } = useContext(LockerDashboardContext);
+
+    const [filename, setFilename] = useState<string>();
+    const [extension, setExtension] = useState<string>();
+
+    useEffect(() => {
+        const [filename, extension] = splitFilenameAndExtension(
+            selectedFiles[0].metadata.title
+        );
+        setFilename(filename);
+        setExtension(extension);
+    }, []);
+
+    const callback: SingleInputFormProps['callback'] = async (
+        inputValue,
+        setFieldError
+    ) => {
+        try {
+            if (inputValue === filename) {
+                props.onHide();
+                return;
+            }
+            const newTitle = getFileTitle(inputValue, extension);
+            let updatedFile = await changeFileName(selectedFiles[0], newTitle);
+            updatedFile = (
+                await updateFilePublicMagicMetadata([updatedFile])
+            )[0];
+            updateExistingFilePubMetadata(selectedFiles[0], updatedFile);
+
+            props.onHide();
+        } catch (e) {
+            setFieldError(e.message);
+        }
+    };
 
     return (
-        <Dialog open={props.show} onClose={props.onHide} maxWidth="xs">
-            <DialogTitleWithCloseButton onClose={props.onHide}>
-                New Collection
-            </DialogTitleWithCloseButton>
-            <DialogContent>
-                <FlexWrapper flexDirection={'column'}>
-                    <Input
-                        placeholder="Collection Name"
-                        onChange={(e) => {
-                            setNewName(e.target.value);
-                        }}
-                    />
-                    <Button
-                        variant="text"
-                        onClick={async () => {
-                            await createCollection(
-                                newName,
-                                CollectionType.folder
-                            );
-                            props.onHide();
-                        }}>
-                        Create
-                    </Button>
-                </FlexWrapper>
-            </DialogContent>
-        </Dialog>
+        <DialogBoxV2
+            open={props.show}
+            onClose={props.onHide}
+            attributes={{
+                title: t('RENAME_FILE'),
+            }}>
+            <SingleInputForm
+                initialValue={selectedFiles[0].title}
+                callback={callback}
+                placeholder={'File name'}
+                buttonText={'Rename'}
+                fieldType="text"
+                caption=""
+                secondaryButtonAction={props.onHide}
+                submitButtonProps={{
+                    sx: {
+                        mt: 1,
+                        mb: 2,
+                    },
+                }}
+            />
+        </DialogBoxV2>
     );
 };
 
