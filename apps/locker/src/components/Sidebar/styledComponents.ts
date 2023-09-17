6b556a088b3106b6e3a24a7caec52ed356f96efe
httpjamesm <github@httpjames.space>
2023-09-16 20:04:49 -0400

    feat: ente-design sidebar

---
+++ apps/locker/src/components/Sidebar/styledComponents.ts
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material';
+
+export const NoStyleAnchor = styled('a')`
+    color: inherit;
+    text-decoration: none !important;
+    &:hover {
+        color: #fff !important;
+    }
+`;
--- apps/locker/src/components/pages/locker/Drawer.tsx
+++ apps/locker/src/components/pages/locker/Drawer.tsx
@@ -1,24 +1,18 @@
 import SubscriptionCard from '@/components/Sidebar/SubscriptionCard';
 import { DrawerSidebar } from '@/components/Sidebar/drawer';
 import { LockerDashboardContext } from '@/pages/locker';
-import {
-    List,
-    ListItem,
-    IconButton,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
-    Box,
-    Typography,
-} from '@mui/material';
-import { Dispatch, SetStateAction, useContext } from 'react';
-import CloseIcon from '@mui/icons-material/Close';
+import { ListItem, Box, Typography, Stack, Divider } from '@mui/material';
+import { Dispatch, SetStateAction, useContext, useMemo } from 'react';
 import CloudIcon from '@mui/icons-material/Cloud';
-import FolderDeleteIcon from '@mui/icons-material/FolderDelete';
 // import SettingsIcon from '@mui/icons-material/Settings';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { t } from 'i18next';
 import { logoutUser } from '@/services/userService';
+import { EnteMenuItem } from '@/components/Menu/EnteMenuItem';
+import HelpSection from '@/components/Sidebar/HelpSection';
+import HeaderSection from '@/components/Sidebar/Header';
+import { openLink } from '@/utils/common';
+import { NoStyleAnchor } from '@/components/Sidebar/styledComponents';
 
 const LockerDrawer = ({
     isOpen,
@@ -31,6 +25,16 @@ const LockerDrawer = ({
         LockerDashboardContext
     );
 
+    const { files, collections } = useContext(LockerDashboardContext);
+
+    const nonTrashFilesCount = useMemo(() => {
+        return files.filter((file) => !file.isTrashed).length;
+    }, [files]);
+
+    const trashFilesCount = useMemo(() => {
+        return files.filter((file) => file.isTrashed).length;
+    }, [files]);
+
     return (
         <DrawerSidebar
             disablePortal
@@ -39,72 +43,69 @@ const LockerDrawer = ({
             onClose={() => {
                 setIsOpen(false);
             }}>
-            <List>
-                <ListItem>
-                    <IconButton
+            <HeaderSection closeSidebar={() => setIsOpen(false)} />
+            <Divider />
+            <Typography padding={1} fontWeight={'medium'} color="text.muted">
+                {userDetails?.email}
+            </Typography>
+            {userDetails && (
+                <Box px={0.5} mt={0.5} pb={1.5} mb={1}>
+                    <SubscriptionCard
+                        userDetails={userDetails}
                         onClick={() => {
-                            setIsOpen(false);
-                        }}>
-                        <CloseIcon />
-                    </IconButton>
-                </ListItem>
-                <ListItem>
-                    <Typography
-                        variant="large"
-                        px={0.5}
-                        fontWeight={'medium'}
-                        color="text.muted">
-                        {userDetails?.email}
-                    </Typography>
-                </ListItem>
-                <ListItem>
-                    {userDetails && (
-                        <Box px={0.5} mt={0.5} pb={1.5} mb={1}>
-                            <SubscriptionCard
-                                userDetails={userDetails}
-                                onClick={() => {
-                                    window.open('https://web.ente.io');
-                                }}
-                            />
-                        </Box>
-                    )}
-                </ListItem>
-                <ListItem>
-                    <ListItemButton
-                        onClick={() => {
-                            setDashboardView('locker');
-                            setIsOpen(false);
-                        }}>
-                        <ListItemIcon>
-                            <CloudIcon />
-                        </ListItemIcon>
-                        <ListItemText primary={t('LOCKER')} />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem>
-                    <ListItemButton
-                        onClick={() => {
-                            setDashboardView('trash');
-                            setIsOpen(false);
-                        }}>
-                        <ListItemIcon>
-                            <FolderDeleteIcon />
-                        </ListItemIcon>
-                        <ListItemText primary={t('TRASH')} />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem>
-                    <ListItemButton
-                        onClick={() => {
-                            logoutUser();
-                        }}>
-                        <ListItemIcon>
-                            <LogoutIcon />
-                        </ListItemIcon>
-                        <ListItemText primary={t('LOGOUT')} />
-                    </ListItemButton>
-                </ListItem>
-                {/* <ListItem>
+                            window.open('https://web.ente.io');
+                        }}
+                    />
+                </Box>
+            )}
+            <Stack spacing={0.5} mb={3}>
+                <EnteMenuItem
+                    startIcon={<CloudIcon />}
+                    label={t('LOCKER')}
+                    onClick={() => {
+                        setDashboardView('locker');
+                        setIsOpen(false);
+                    }}
+                    variant="captioned"
+                    subText={(
+                        nonTrashFilesCount + collections.length
+                    ).toString()}
+                />
+                <EnteMenuItem
+                    startIcon={<CloudIcon />}
+                    label={t('TRASH')}
+                    onClick={() => {
+                        setDashboardView('trash');
+                        setIsOpen(false);
+                    }}
+                    variant="captioned"
+                    subText={trashFilesCount.toString()}
+                />
+                <EnteMenuItem
+                    labelComponent={
+                        <NoStyleAnchor href="https://web.ente.io">
+                            <Typography fontWeight={'bold'}>
+                                {t('ACCOUNT_SETTINGS')}
+                            </Typography>
+                        </NoStyleAnchor>
+                    }
+                    variant="secondary"
+                    onClick={() => openLink('https://web.ente.io', true)}
+                />
+
+                <Divider />
+                <HelpSection />
+                <Divider />
+                <EnteMenuItem
+                    startIcon={<LogoutIcon />}
+                    label={t('LOGOUT')}
+                    onClick={logoutUser}
+                    color="critical"
+                    variant="secondary"
+                />
+            </Stack>{' '}
+            {/* </Stack> */}
+            {/* <ListItem>
                     <ListItemButton>
                         <ListItemIcon>
                             <SettingsIcon />
@@ -112,7 +113,7 @@ const LockerDrawer = ({
                         <ListItemText primary="Settings" />
                     </ListItemButton>
                 </ListItem> */}
-            </List>
+            {/* </List> */}
         </DrawerSidebar>
     );
 };
