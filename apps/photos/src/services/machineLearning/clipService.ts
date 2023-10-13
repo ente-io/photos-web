f43f2a32a97c073d10eeb88939e4b59bd4a55b82
Abhinav <abhinavk.grd@gmail.com>
2023-10-13 11:53:32 +0530

    add textEmbedding extraction and computeScore functions

--- apps/photos/src/services/machineLearning/clipService.ts
+++ apps/photos/src/services/machineLearning/clipService.ts
@@ -2,13 +2,13 @@ import { MLSyncContext, MLSyncFileContext } from 'types/machineLearning';
 import { isDifferentOrOld } from 'utils/machineLearning';
 import ReaderService from './readerService';
 import { addLogLine } from 'utils/logging';
+import mlIDbStorage from 'utils/storage/mlIDbStorage';
 
 class ClipService {
-    async syncFileClipImageEmbeddings(
+    async syncFileClipImageEmbedding(
         syncContext: MLSyncContext,
         fileContext: MLSyncFileContext
     ) {
-        const startTime = Date.now();
         const { oldMlFile, newMlFile } = fileContext;
         if (
             !isDifferentOrOld(
@@ -29,11 +29,14 @@ class ClipService {
             syncContext,
             fileContext
         );
-        const clipImageEmbeddingExtractionResult =
-            await syncContext.clipService.computeImageEmbeddings(imageBitmap);
+        const startTime = Date.now();
+
+        const embedding = await syncContext.clipService.computeImageEmbedding(
+            imageBitmap
+        );
 
         newMlFile.ClipImageEmbedding = {
-            ...clipImageEmbeddingExtractionResult,
+            embedding,
             fileId: fileContext.enteFile.id,
         };
         const endTime = Date.now();
@@ -42,6 +45,51 @@ class ClipService {
                 fileContext.enteFile.id
             }: ${endTime - startTime}ms`
         );
+    }
+
+    async generateTextEmbedding(syncContext: MLSyncContext, text: string) {
+        const startTime = Date.now();
+        const embedding = await syncContext.clipService.computeTextEmbedding(
+            text
+        );
+        const endTime = Date.now();
+        addLogLine(
+            `Clip computeTextEmbeddings time taken ${text}: ${
+                endTime - startTime
+            }ms`
+        );
+        return embedding;
+    }
+
+    async getAllClipImageEmbeddings() {
+        return await mlIDbStorage.getAllClipImageEmbeddings();
+    }
+
+    async computeScore(
+        imageEmbedding: Float32Array,
+        textEmbedding: Float32Array
+    ) {
+        if (imageEmbedding.length !== textEmbedding.length) {
+            throw Error('imageEmbedding and textEmbedding length mismatch');
+        }
+        let score = 0;
+        let imageNormalization = 0;
+        let textNormalization = 0;
+
+        for (let index = 0; index < imageEmbedding.length; index++) {
+            imageNormalization += imageEmbedding[index] * imageEmbedding[index];
+            textNormalization += textEmbedding[index] * textEmbedding[index];
+        }
+        for (let index = 0; index < imageEmbedding.length; index++) {
+            imageEmbedding[index] =
+                imageEmbedding[index] / Math.sqrt(imageNormalization);
+            textEmbedding[index] =
+                textEmbedding[index] / Math.sqrt(textNormalization);
+        }
+        for (let index = 0; index < imageEmbedding.length; index++) {
+            score += imageEmbedding[index] * textEmbedding[index];
+        }
+        return score;
     }
 }
 
