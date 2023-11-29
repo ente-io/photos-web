1aa832c6f01da48aa48bc923519b5e954aaa92a4
Neeraj Gupta <neeraj.gupta983@gmail.com>
2023-11-29 10:49:37 +0530

    Add `web:` as prefix

--- packages/accounts/api/user.ts
+++ packages/accounts/api/user.ts
@@ -26,8 +26,7 @@ export const sendOtt = (appName: APPS, email: string) => {
 };
 
 export const verifyOtt = (email: string, ott: string, referral: string) => {
-    let cleanedReferral = referral?.trim() || '';
-    cleanedReferral += ':web';
+    const cleanedReferral = `web:${referral?.trim() || ''}`;
     return HTTPService.post(`${ENDPOINT}/users/verify-email`, {
         email,
         ott,
