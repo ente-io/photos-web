89192dd6f47b71bfea91fc8b939fba73c6ea13c6
Abhinav <abhinavk.grd@gmail.com>
2024-01-26 18:41:45 +0530

    update sentry dsn

--- apps/auth/sentry.client.config.ts
+++ apps/auth/sentry.client.config.ts
@@ -1,6 +1,6 @@
 import { setupSentry } from '@ente/shared/sentry/config/sentry.config.base';
 
 const DEFAULT_SENTRY_DSN =
-    'https://e2ccc39d811640b49602323774220955@sentry.ente.io/13';
+    'https://6061cb842359ec43be9def9f63a5a2f5@sentry.ente.io/10';
 
 setupSentry(DEFAULT_SENTRY_DSN);
--- apps/photos/sentry.client.config.ts
+++ apps/photos/sentry.client.config.ts
@@ -1,5 +1,5 @@
 import { setupSentry } from '@ente/shared/sentry/config/sentry.config.base';
 
 const DEFAULT_SENTRY_DSN =
-    'https://bd3656fc40d74d5e8f278132817963a3@sentry.ente.io/2';
+    'https://1a6aff9f51ca99e9e45524253e5457af@sentry.ente.io/2';
 setupSentry(DEFAULT_SENTRY_DSN);
